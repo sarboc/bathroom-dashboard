ae8b9df45bab5321ba8f0bcef3761fb10d1d8084
Sara Bocciardi <sara@fandor.com>
2015-09-18 21:58:36 -0700

    Removing next from data send.

--- jobs/forecast.rb
+++ jobs/forecast.rb
@@ -29,5 +29,5 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   forecast_current_desc = forecast["currently"]["summary"]
   forecast_later_desc   = forecast["hourly"]["summary"]
   forecast_later_icon   = forecast["hourly"]["icon"]
-  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", next_icon: "#{forecast_next_icon}", next_desc: "#{forecast_next_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
+  send_event('forecast', { current_temp: "#{forecast_current_temp}&deg;", current_icon: "#{forecast_current_icon}", current_desc: "#{forecast_current_desc}", later_icon: "#{forecast_later_icon}", later_desc: "#{forecast_later_desc}"})
 end
