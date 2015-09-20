9bc2bf6c7a0d49b9bffa3bc0473ecba730abca5f
Sara Bocciardi <sara@fandor.com>
2015-09-20 16:40:32 -0700

    Skipping images if they're broken for some reason until we find one with an image embed.

--- jobs/big_image.rb
+++ jobs/big_image.rb
@@ -16,14 +16,20 @@ SCHEDULER.every '1m', :first_in => 0 do
     gallery_images = gallery.css("div.post-image")
   end
 
-  image = gallery_images.shift
+  image_url = nil
 
-  if !image.css("img").empty?
-    image_url = image.css("img")[0].attributes["src"].value
-  elsif !image.css("source").empty?
-    image_url = image.css("source")[1].attributes["src"].value
-  else
-    image_url = "//i.imgur.com/0mAflZ7.jpg"
+  while image_url.nil?
+    if gallery_images.empty?
+      image_url = "i.imgur.com/0mAflZ7.jpg"
+    else
+      image = gallery_images.shift
+    end
+
+    if !image.css("img").empty?
+      image_url = image.css("img")[0].attributes["src"].value
+    elsif !image.css("source").empty?
+      image_url = image.css("source")[1].attributes["src"].value
+    end
   end
 
   send_event('picture', { image: "http:#{image_url}"})
