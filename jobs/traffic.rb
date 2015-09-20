988939f862ec551c4f94d01053198b09665f88d6
Sara Bocciardi <sara@fandor.com>
2015-09-20 16:29:03 -0700

    Hooking up traffic widget to the API and styling tweaks.

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,10 +1,11 @@
-"https://maps.googleapis.com/maps/api/distancematrix/output?parameters"#!/usr/bin/env ruby
-require 'icalendar'
+require 'net/http'
+require 'openssl'
 
 json = File.read('config.json')
 secret_config = JSON.parse(json)['traffic']
 
-base_url = "https://maps.googleapis.com/maps/api/distancematrix/"
+base_url = 'maps.googleapis.com'
+base_api_url = '/maps/api/distancematrix/json?'
 
 google_api_key = secret_config['google_api_key']
 
@@ -17,13 +18,30 @@ sara_destination_long = secret_config['sara_destination_long']
 sean_destination_lat = secret_config['sean_destination_lat']
 sean_destination_long = secret_config['sean_destination_long']
 
-request_url = "#{base_url}"
+distances = {
+  sara: [origin_lat, origin_long, sara_destination_lat, sara_destination_long],
+  sean: [origin_lat, origin_long, sean_destination_lat, sean_destination_long]
+}
+
+request_urls = {}
+
+distances.each do |person, vals|
+  request_urls[person] = "#{base_api_url}origins=#{vals[0]},#{vals[1]}&destinations=#{vals[2]},#{vals[3]}&key=#{google_api_key}"
+end
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  # req = Net::HTTP::Get.new(request_url)
-  # result = http.request(req).body
+  results = {}
 
-  sara_time = "30 min"
-  sean_time = "20 min"
-  send_event('traffic', { sara_time: sara_time, sean_time: sean_time })
+  request_urls.each do |person, url|
+    http = Net::HTTP.new(base_url, 443)
+    http.use_ssl = true
+    http.ca_file = 'lib/ca-bundle.crt'
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    req = Net::HTTP::Get.new(url)
+    result = JSON.parse(http.request(req).body)
+    seconds = result['rows'][0]['elements'][0]['duration']['value']
+    results[person] = (seconds / 60).round
+  end
+
+  send_event('traffic', { sara_time: results[:sara], sean_time: results[:sean] })
 end
