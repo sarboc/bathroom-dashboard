46cd4e23e2cf518c2420baabff6b89f2cfefb5d1
Sara Bocciardi <sara@fandor.com>
2015-09-25 21:02:24 -0700

    Renaming current calendar to sara_personal in order to add second calendar.

--- jobs/google_calendar.rb
+++ jobs/google_calendar.rb
@@ -13,24 +13,12 @@ SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
 
 json = File.read('config.json')
 secret_config = JSON.parse(json)['google_calendar']
-client_id = secret_config['client_id']
-client_secret = secret_config['client_secret']
 service_account_email = secret_config['service_account_email'] # Email of service account
 key_file = secret_config['key_file'] # File containing your private key
 key_secret = secret_config['key_secret'] # Password to unlock private key
-calendarID = secret_config['sara_personal_calendar_id'] # Calendar ID.
+sara_personal_calendar_id = secret_config['sara_personal_calendar_id'] # Calendar ID.
 
 ENV['SSL_CERT_FILE'] = 'lib/ca-bundle.crt'
-
-# auth = {
-#   "web" => {
-#     "client_id" => client_id,
-#     "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
-#     "token_uri" => "https://accounts.google.com/o/oauth2/token",
-#     "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
-#     "client_secret" => client_secret
-#   }
-# }
 
 # Get the Google API client
 client = Google::APIClient.new(application_name: "Bathroom Dashboard", application_version: "0.0.1")
@@ -59,8 +47,11 @@ SCHEDULER.every '15m', :first_in => 4 do |job|
 
   base_options = {'timeMin' => now.rfc3339, 'orderBy' => 'startTime', 'singleEvents' => 'true', 'maxResults' => 3}
 
-  result = client.execute(api_method: service.events.list, parameters: base_options.merge({'calendarId' => calendarID}))
+  result = client.execute(
+    api_method: service.events.list,
+    parameters: base_options.merge({'calendarId' => sara_personal_calendar_id})
+  )
 
-  send_event('google_calendar', { events: result.data })
+  send_event('sara_personal_calendar', { events: result.data })
 
 end
